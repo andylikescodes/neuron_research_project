640afbc43285c4a5e259538ab2441c3789e07e4a
Dehua Liang <andylikescodes@gmail.com>
2018-06-30 18:35:22 -0700

    Finish the NOData class and cell class for data access. Code needed to be fined and later on analysis classes and plotting classes will be added.

--- data.py
+++ data.py
@@ -71,12 +71,27 @@ class NOData:
         stimulus_off_events = events.loc[events[1] == self._markers['stimulus_off']]
         question_on_events = events.loc[events[1] == self._markers['delay1_off']]
         trial_end_events = events.loc[events[1] == self._markers['delay2_off']]
+        recog_response_events = events.loc[(events[1] == self._markers['response_1']) |
+                                           (events[1] == self._markers['response_2']) |
+                                           (events[1] == self._markers['response_3']) |
+                                           (events[1] == self._markers['response_4']) |
+                                           (events[1] == self._markers['response_5']) |
+                                           (events[1] == self._markers['response_6'])]
+
         if (stimulus_on_events.shape[0] != 100) | (stimulus_off_events.shape[0] != 100) | \
            (question_on_events.shape[0] != 100) | (trial_end_events.shape[0] != 100):
             print('Somethings wrong with the event data, each event should have 100 trials.')
             raise ValueError
+
+        events_time_points = pd.DataFrame({'stimulus_on': np.asarray(stimulus_on_events[0]),
+                                           'stimulus_off': np.asarray(stimulus_off_events[0]),
+                                           'question_on': np.asarray(question_on_events[0]),
+                                           'trial_end': np.asarray(trial_end_events[0]),
+                                           # raw response from 31 - 36, designed response 1 - 6
+                                           'recog_responses': np.asarray(recog_response_events[1] - 30)})
         # TODO experiment_type = 'learn'
-        return stimulus_on_events, stimulus_off_events, question_on_events, trial_end_events
+        #print(events_time_points)
+        return events_time_points
 
     def _get_event_data(self, session_nr, experiment_type='recog'):
         """
@@ -88,7 +103,6 @@ class NOData:
         event_path = os.path.join(self._construct_data_path(session_name, 'events'), 'eventsRaw.mat')
 
         events = pd.DataFrame(loadmat(event_path)['events'])
-        print(events.shape)
         if experiment_type == 'recog':
             events = events.loc[events[2] == experiment_id_recog]
         elif experiment_type == 'learn':
@@ -99,28 +113,104 @@ class NOData:
         print(events.tail())
         return events
 
-    def _get_trials_spike_timestamps(self, cell, experiment_type='recog'):
+    def get_trials_from_cell(self, cell, experiment_type='recog'):
+        """
+        This method takes in the cell raw spike data and separate it into different trials based on the event
+        timestamps.
+        :param cell: A cell object contains the cell information including the raw spike train.
+        :param experiment_type: TODO: learning = 'learning' or recognition phase = 'recog'
+        :return: A 100 x 7 list with columns indexes:
+                 0 = category_recog_number
+                 1 = category_recog_desc
+                 2 = img_file_path
+                 3 = stimuli_id
+                 4 = new_old_recog
+                 5 = Response dataframe for scale from 1 - 6 for the recognition phase,
+                 6 = a list contains the spikes for each trials, 100 trials.
+        """
+
+        # get labels from cell
+        trial_label_recog = self._get_labels_from_cell(cell)
+
         session_nr = cell.session_nr
         events = self._get_event_data(session_nr, experiment_type=experiment_type)
-        stimulus_on_events, stimulus_off_events, question_on_events, trial_end_events = \
-            self._extract_event_periods(events, experiment_type=experiment_type)
+        events_time_points = self._extract_event_periods(events, experiment_type=experiment_type)
+        recog_responses = events_time_points['recog_responses']
 
-        cell_spike_timestamps = pd.DataFrame(cell.spike_timestamps)
-
-        spike_trials = []
-        print(spike_trials)
+        cell_raw_spike_timestamps = pd.DataFrame(cell.raw_spike_timestamps)
         for i in range(0, 100):
-            trial_start = stimulus_on_events.iloc[i, 0]
-            trial_end = trial_end_events.iloc[i, 0]
-            trial = cell_spike_timestamps.loc[(cell_spike_timestamps[0] > trial_start) &
-                                              (cell_spike_timestamps[0] < trial_end)]
+            trial_start = events_time_points.iloc[i, 0]
+            trial_end = events_time_points.iloc[i, 3]
+            trial = cell_raw_spike_timestamps.loc[(cell_raw_spike_timestamps[0] > trial_start) &
+                                                  (cell_raw_spike_timestamps[0] < trial_end)]
             trial = trial[0].tolist()
-            spike_trials.append(np.asarray(trial))
-        return spike_trials
+            trial_label_recog[i].append(recog_responses[i])
+            trial_label_recog[i].append(np.asarray(trial) - trial_start)
+        return trial_label_recog
+
+    def _get_labels_from_cell(self, cell, experiment_type='recog'):
+        """
+        This method use the original experiment stimuli data set to extract the order and categories of the stimuli
+        shown to each subjects. The label of the stimulus for the new old recognition task is also obtained using
+        this method.
+        :param cell: a cell object contains the session number information to extract necessary data from the session
+                     dictionary.
+        :param experiment_type: 'recog' = recognition phase, 'learn' = learning phase
+        :return: a 100 x 5 list with columns 0 = category_recog_number, 1 = category_recog_desc, 2 = img_file_path,
+                 3 = stimuli_id, 4 = new_old_recog
+        """
+        session_nr = cell.session_nr
+        block_id_recog = self.sessions[session_nr]['block_id_recog']
+        block_id_learn = self.sessions[session_nr]['block_id_learn']
+        variant = self.sessions[session_nr]['variant']
+        filename = 's'
+        if variant == 1:
+            filename = 'NewOldDelay_v3.mat'
+            filename2 = 'NewOldDelayStimuli.mat'
+        elif variant == 2:
+            filename = 'NewOldDelay2_v3.mat'
+            filename2 = 'NewOldDelayStimuli2.mat'
+        elif variant == 3:
+            filename = 'NewOldDelay3_v3.mat'
+            filename2 = 'NewOldDelayStimuli3.mat'
+
+        path_to_labels = os.path.join(self.path, 'stimFiles', filename)
+        experiment_stimuli = loadmat(path_to_labels)['experimentStimuli']
+        stimuli_recog = experiment_stimuli[0, block_id_recog-1][3]
+        new_old_recog = experiment_stimuli[0, block_id_recog-1][4]
+        stimuli_learn = experiment_stimuli[0, block_id_learn-1][2]
+
+        path_to_categories = os.path.join(self.path, 'stimFiles', filename2)
+        category_mat = loadmat(path_to_categories)
+        category = category_mat['categories']
+        category_mapping = pd.DataFrame(category_mat['categoryMapping']).set_index(0)
+        file_mapping = category_mat['fileMapping']
+
+        category_file_label_recog = []
+        for i in range(0, 100):
+            temp = []
+            stimuli_recog_value = stimuli_recog[0][i]
+            category_number = category_mapping.loc[stimuli_recog_value, 1]
+            temp.append(category_number)
+            temp.append(category[0, category_number-1][0])
+            file_path = file_mapping[0][i][0].replace('C:\code\\', '')
+            temp.append(file_path)
+            temp.append(stimuli_recog_value)
+            temp.append(new_old_recog[0][i])
+
+            category_file_label_recog.append(temp)
+
+
+
+        # print(category.shape)
+        # print(category_mapping)
+        # print(file_mapping.shape)
+
+        return category_file_label_recog
 
     def _define_session(self):
         """ 
-            Create a dictionary to contain the usable session information.
+        Create a dictionary to contain the usable session information.
         """
 
         # read in session info
@@ -159,13 +249,13 @@ class NOData:
 
     def pop_cell(self, session_nr, channelnr_clusterid):
         """
-            This method pops a particular cell to the user
-            input:
-                session_nr = the session number that we would like to use
-                channelnr_clusterid = the tuple contains both the channel id and the cluster id to select the 
-                                        desired cell
-            output:
-                cell = a cell object
+        This method pops a particular cell to the user
+        input:
+            session_nr = the session number that we would like to use
+            channelnr_clusterid = the tuple contains both the channel id and the cluster id to select the
+                                    desired cell
+        output:
+            cell = a cell object
         """
 
         session_name = self.sessions[session_nr]['session']
@@ -184,9 +274,13 @@ class NOData:
     def test(self):
         cell = self.pop_cell(114, (17, 1))
 
-        trials = self._get_trials_spike_timestamps(cell)
-        for i in range(len(trials)):
-            print(len(trials[i]))
+        #trials, recog_response = self.get_trials_from_cell(cell)
+
+        stimuli_recog, new_old_recog = self.get_labels_from_cell(cell)
+
+        # print(recog_response)
+        # print(stimuli_recog)
+        # print(new_old_recog)
 
     # Static helper methods
     @staticmethod
@@ -254,7 +348,7 @@ class Cell:
         self._cluster_id = cell_info[1]
         self._ori_cluster_id = cell_info[2]
         self._brain_area_id = cell_info[3]
-        self._spike_timestamps = self._load_cell_data(cell_path)
+        self._raw_spike_timestamps = self._load_cell_data(cell_path)
 
     @property
     def data_path(self):
@@ -285,18 +379,18 @@ class Cell:
         return self._brain_area_id
 
     @property
-    def spike_timestamps(self):
-        return self._spike_timestamps
+    def raw_spike_timestamps(self):
+        return self._raw_spike_timestamps
 
     def _load_cell_data(self, cell_path):
         """
         load the raw cell data and capture the spike train timestamps
         """
         cell_data = loadmat(cell_path)
-        channel_spike_timestamps = pd.DataFrame(cell_data['spikes'])
-        cell_spike_timestamps = np.asarray(
-            channel_spike_timestamps.loc[channel_spike_timestamps[0] == self.cluster_id, 2])
-        return cell_spike_timestamps
+        channel_raw_spike_timestamps = pd.DataFrame(cell_data['spikes'])
+        cell_raw_spike_timestamps = np.asarray(
+            channel_raw_spike_timestamps.loc[channel_raw_spike_timestamps[0] == self.cluster_id, 2])
+        return cell_raw_spike_timestamps
 
 
 

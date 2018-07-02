d819f0ca087c176721eedbc15a95a2d62f5f5338
Dehua Liang <andylikescodes@gmail.com>
2018-07-01 17:46:42 -0700

    Added a trial class

--- data.py
+++ data.py
@@ -393,7 +393,48 @@ class Cell:
         return cell_raw_spike_timestamps
 
 
+class Trial:
+    def __init__(self, category, new_old_recog, response_recog, category_name,
+                 file_path, stimuli_id, trial_duration, trial_timestamps):
+        self._category = category
+        self._new_old_recog = new_old_recog
+        self._response_recog = response_recog
+        self._category_name = category_name
+        self._file_path = file_path
+        self._stimuli_id = stimuli_id
+        self._trial_duration = trial_duration
+        self._trial_timestamps = trial_timestamps
 
+    @property
+    def category(self):
+        return self._category
 
+    @property
+    def new_old_recog(self):
+        return self._new_old_recog
+
+    @property
+    def response_recog(self):
+        return self._response_recog
+
+    @property
+    def category_name(self):
+        return self._category_name
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @property
+    def stimuli_id(self):
+        return self._stimuli_id
+
+    @property
+    def trial_duration(self):
+        return self._trial_duration
+
+    @property
+    def trial_timestamps(self):
+        return self._trial_timestamps
 
 

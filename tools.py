bfc2ed768b52eacd09176e8a5dc79eac3d4b8b30
Dehua Liang <andylikescodes@gmail.com>
2018-07-06 14:01:05 -0700

    Started the classifier

--- tools.py
+++ tools.py
@@ -1,1 +1,10 @@
 # All analyzing tools
+
+class MaxCorrelationClassifier:
+    def __init__(self):
+        pass
+    def fit(self, X, y):
+        # senaty check:
+        if len(y) != X.shape[1]:
+            print('The number of records of X must equal the number of labels')
+
